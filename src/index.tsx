8326c40dc2a6007b80ff27605fb80b33e75f497f
bguillen <bguillen@truechoice.io>
2021-12-22 13:39:33 -0500

    feat: add TextInput

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import { Button } from './button/Button';
 import { Header } from './header/Header';
+import { TextInput } from './text-input/TextInput';
 
 export {
   Button,
-  Header
+  Header,
+  TextInput
 };
