07e65734acec7c169aeae7bd853c11b29b0ffadc
bguillen <bguillen@truechoice.io>
2021-12-22 14:47:22 -0500

    feat: deprecate className

--- src/text-input/TextInput.tsx
+++ src/text-input/TextInput.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 interface TextInputProps {
-  className: string
   id: string
   label: string
   name: string
 }
 
 const TextInput = ({
-  className,
   id,
   label,
   name
 }: TextInputProps) => {
   return (
-    <div className={className}>
+    <div>
       <label style={{ display: 'block' }} htmlFor={name}>{label}</label>
       <input name={name} id={id}/>
     </div>
